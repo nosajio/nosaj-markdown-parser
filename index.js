b92ab69db253634206376d8a9b6afb844d87bc91
Jason Howmans <jason@nosaj.io>
2018-01-17 20:30:11 +0000

    I'm an idiot

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const helpers = require('./helpers');
 
-const export = Object.assign({}, helpers, {
+const exportable = Object.assign({}, helpers, {
   fileOpener:     require('./file-opener'),
   markdownParser: require('./markdown-parser'),
 });
 
-module.exports = Object.freeze( export );
+module.exports = Object.freeze( exportable );
